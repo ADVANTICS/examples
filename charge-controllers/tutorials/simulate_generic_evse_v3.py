e57ceb1156e49df7b9b0e6d441fedba56f0c49e0
Hedwyn <bpestourie@gmail.com>
2025-01-10 12:24:10 +0100

    add standalone scripts to run the simulations locally without needed the package installed

---
+++ charge-controllers/tutorials/simulate_generic_evse_v3.py
@@ -0,0 +1,7 @@
+"""
+Standalone script to run EVSE simulation locally without
+installing the package
+"""
+from advsimulators.evse.generic_v3 import cli_main
+cli_main()
+
---
+++ charge-controllers/tutorials/simulate_generic_pev_v2.py
@@ -0,0 +1,7 @@
+"""
+Standalone script to run PEV simulation locally without
+installing the package
+"""
+from advsimulators.pev.generic_v2 import cli_main
+cli_main()
+
